cc114d0a64580d5f4341793bee1030690469fe49
Alex <78886791+alexander2475@users.noreply.github.com>
2025-01-25 17:48:31 -0700

    small change

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
+import {
+    vitePreprocess
+} from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+            base: process.env.NODE_ENV === 'production' ? '/workfish2475.github.io' : '',
+        }
+    }
 };
 
 export default config;
